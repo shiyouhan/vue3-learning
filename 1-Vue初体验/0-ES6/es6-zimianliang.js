505dce3cf26fb7c64610980c895d7b17c7be0049
shiyouhan <2470752581@qq.com>
2022-03-10 10:28:06 +0800

    vue3--课堂学习

---
+++ 1-Vue初体验/0-ES6/es6-zimianliang.js
@@ -0,0 +1,8 @@
+let name = "Tom";
+let age = 21;
+
+let player1 = {
+    name, age
+}
+
+console.log(player1);
---
+++ 1-Vue初体验/0-ES6/this-obj.js
@@ -0,0 +1,7 @@
+// 对象中的thi指针
+class Player {
+    // 定义构造函数
+    constructor(name, age) {
+        
+    }
+}
